09f066bcc6aacf0b2cf9a68b571b0e22110be2f0
Ujjwal Sharma <sharmaujjwal123456789@gmail.com>
2025-11-01 20:24:43 +0530

    better approaxch

--- Arr19.java
+++ Arr19.java
@@ -120,24 +120,62 @@
 // }
 
 //  optimal approach for number that appear once 
-import java.util.Scanner;
-public class Arr19{
-    public static void getSingle(int []arr,int n){
-        int xor=0;
-        for(int i=0;i<arr.length;i++){
-            xor=xor^arr[i];
-        }
-        System.out.println(xor);
-    }
-    public static void main(String[] args){
-  Scanner sc= new Scanner(System.in);
-  int n =sc.nextInt();
-  int []arr=new int[n];
+// import java.util.Scanner;
+// public class Arr19{
+//     public static void getSingle(int []arr,int n){
+//         int xor=0;
+//         for(int i=0;i<arr.length;i++){
+//             xor=xor^arr[i];
+//         }
+//         System.out.println(xor);
+//     }
+//     public static void main(String[] args){
+//   Scanner sc= new Scanner(System.in);
+//   int n =sc.nextInt();
+//   int []arr=new int[n];
 
-  System.out.println("Enter the"+n+"Integer ");
-  for(int i=0;i<n;i++){
-    arr[i]=sc.nextInt();
-  }
-  getSingle(arr,n);
-    }
-}
+//   System.out.println("Enter the"+n+"Integer ");
+//   for(int i=0;i<n;i++){
+//     arr[i]=sc.nextInt();
+//   }
+//   getSingle(arr,n);
+//     }
+// }
+
+//  better epproach for single element
+// import java.util.Scanner;
+// public class Arr19{
+//     public static int getsingle(int[] arr,int n){
+//         int maxi=arr[0];
+//         for(int i=1;i<arr.length;i++){
+//             maxi=Math.max(maxi,arr[i]);
+//         }
+
+//         int[]hash=new int[maxi + 1];
+
+//         for(int i=0;i<n;i++){
+//             hash[arr[i]]++;
+//         }
+
+//         for(int i=0;i<n;i++){
+//          if(hash[arr[i]] == 1){
+//             return arr[i];
+//          }
+//         }
+//         return -1;
+//     }
+
+//     public static void main(String[]args){
+//         Scanner sc=new Scanner(System.in);
+
+//         int n=sc.nextInt();
+//         int []arr=new int[n];
+//         System.out.println("Enter the  integer");
+
+//         for(int i=0;i<n;i++){
+//             arr[i]=sc.nextInt();
+//         }
+//         int result=getsingle(arr,n);
+//         System.out.println("the single element is"+ result);
+//     }
+// }
