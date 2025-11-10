b3ddf6e0c5b30a4083e6f2eedb495bed0d7c8005
Ujjwal Sharma <sharmaujjwal123456789@gmail.com>
2025-11-11 00:10:20 +0530

    Arr31

--- Arr30.java
+++ Arr30.java
@@ -48,7 +48,7 @@
 //     public static void zeroMatrix(int [][] matrix){
 //         int n =matrix.length;  // for rows
 //         int m = matrix[0].length;  // for columns
-        
+
 //         int []row = new int[n];
 //         int []col = new int[m];
 
@@ -82,5 +82,68 @@
 //    for(int i=0;i<matrix.length ;i++){
 //     System.out.println(Arrays.toString(matrix[i]));
 //    }
+//     }
+// }
+
+
+// optimal approach for the set matrix zero 
+
+// import java.util.*;
+// public class Arr30{
+
+//     public static ArrayList<ArrayList<Integer>> zeroMatrix(ArrayList<ArrayList<Integer>> matrix){
+//          int n =matrix.size();
+//          int m =matrix.get(0).size();
+
+//          int col0 = 1;
+
+//         for(int i =0 ;i<n ;i++){
+//             for(int j=0 ;j<m ;j++){
+//                 if(matrix.get(i).get(j) == 0){
+//                     matrix.get(i).set(0, 0);
+
+//                     if(j != 0){
+//                         // matrix[0][j] =0;/
+//                         matrix.get(0).set(j, 0);
+//                     }else{
+//                         col0 = 0;
+//                     }
+//                 }
+//             }
+//         }
+//             for(int i=1 ;i < n ;i++){
+//                 for(int j=1 ;j<m; j++){
+
+//                     if(matrix.get(i).get(0) == 0 || matrix.get(0).get(j) == 0){
+//                         matrix.get(i).set(j, 0);
+//                     }
+//                 }
+//             }
+        
+//         if(matrix.get(0).get(0) == 0){
+//             for(int j=0 ;j<m ;j++){
+//                 matrix.get(0).set(j, 0);
+//             }
+//             if(col0 == 0){
+//                 for(int i=0;i<n ;i++){
+//                     matrix.get(i).set(0, 0);
+//                 }
+//             }
+//         }
+//             return matrix;
+//     }
+//     public static void main(String[] args){
+//         ArrayList<ArrayList<Integer>> matrix =new ArrayList<>();
+//       matrix.add(new ArrayList<>(Arrays.asList(1,1,1)));
+//       matrix.add(new ArrayList<>(Arrays.asList(1,0,1)));
+//       matrix.add(new ArrayList<>(Arrays.asList(1,1,1)));
+
+//       zeroMatrix(matrix);
+
+
+//       System.out.println("\nAfter");
+//       for(ArrayList<Integer> row : matrix){
+//         System.out.println(row);
+//       }
 //     }
 // }
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Arr31 {
+    public static int[][] rotate(int[][] matrix){
+     int n =matrix.length;
+     int [][] rotated = new int[n][n];
+
+     for(int i=0;i<n;i++){
+        for(int j=0;j < n;j++){
+            rotated[j][n-i-1] = matrix[i][j];
+        }
+     }
+     return rotated;
+    }
+    public static void main(String[] args){
+   int [][] matrix ={
+    {1,2,3};
+    {4,5,6};
+    {7,8,9};
+   };
+   int [][] result =rotate(matrix);
+
+   System.out.println("Rotated Matrix");
+   for(int i=0 ;i < result.length ;i++){
+    System.out.println(Arrays.toString(result[i]));
+     }
+   } 
+}
