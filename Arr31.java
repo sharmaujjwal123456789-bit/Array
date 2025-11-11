badc30f4881391c7c6ae6bb0d88106c5997f45a8
Ujjwal Sharma <sharmaujjwal123456789@gmail.com>
2025-11-11 12:16:52 +0530

    arr32 spiral matrix

--- Arr31.java
+++ Arr31.java
@@ -64,3 +64,57 @@
       
 //     }
 // } 
+
+
+// Arr 32  spiral traversal of a matrix
+
+import java.util.*;
+
+public class Arr31{
+    public static ArrayList<Integer> spiralMatrix(ArrayList<ArrayList<Integer>> mat){
+    int n =mat.size();
+    int m =mat.get(0).size();
+    int left=0 ,right =m-1;
+    int top =0, bottom=n-1;
+
+    ArrayList<Integer> ans= new ArrayList<>();
+
+    while(top <= bottom && left <= right){
+        for(int i =left ;i <= right ;i++){
+            ans.add(mat.get(top).get(i));
+        }
+        top++;
+
+        for(int i=top ;i<=bottom ;i++){
+            ans.add(mat.get(i).get(right));
+        }
+        right--;
+
+        if(top <= bottom){
+            for(int i= right; i>= left ;i--){
+                ans.add(mat.get(bottom).get(i));
+            }
+            bottom--;
+        }
+
+        if(left <= right){
+            for(int i= bottom ;i>= top ;i--){
+                ans.add(mat.get(i).get(left));
+            }
+            left++;
+        }
+    }
+     return ans;
+    }
+
+    public static void main(String[] args){
+    ArrayList<ArrayList<Integer>> mat = new ArrayList<>();
+        mat.add(new ArrayList<>(Arrays.asList(1, 2, 3)));
+        mat.add(new ArrayList<>(Arrays.asList(4, 5, 6)));
+        mat.add(new ArrayList<>(Arrays.asList(7, 8, 9)));
+
+        ArrayList<Integer> result = spiralMatrix(mat);
+
+        System.out.println("Spiral Order: " + result);
+    }
+}
