44e880bfe0cda0f315a73f2d5370dc44222b9183
Ujjwal Sharma <sharmaujjwal123456789@gmail.com>
2025-11-11 12:01:15 +0530

    Arr31

--- Arr31.java
+++ Arr31.java
@@ -26,3 +26,41 @@
 //      }
 //    } 
 // }
+
+
+//  optimal approach for it 
+// import java.util.*;
+// public class Arr31{
+//     public static ArrayList<ArrayList<Integer>>  rotateMatrix(ArrayList<ArrayList<Integer>> mat){
+//         int n =mat.size();
+
+//         for(int i=0;i<n-1 ;i++){
+//             for(int j=i+1 ;j< n;j++){
+//                 // swap(mat[i][j] ,mat[j][i] );
+//                 int temp = mat.get(i).get(j);
+//                 mat.get(i).set(j ,mat.get(j).get(i));
+//                 mat.get(j).set(i, temp);
+//             }
+//         }
+//         for(int i =0 ;i<n;i++){
+//            Collections.reverse(mat.get(i));
+//         }
+//         return mat;
+//     }
+//     public static void main(String[] args){
+//         ArrayList<ArrayList<Integer>> mat = new ArrayList<>();
+
+//         mat.add(new ArrayList<>(Arrays.asList(1, 2, 3)));
+//         mat.add(new ArrayList<>(Arrays.asList(4, 5, 6)));
+//         mat.add(new ArrayList<>(Arrays.asList(7, 8, 9)));
+
+//         rotateMatrix(mat);  // call function
+
+//         System.out.println("\nRotated Matrix:");
+//         for (ArrayList<Integer> row : mat) {
+//             System.out.println(row);
+//         }
+
+      
+//     }
+// } 
