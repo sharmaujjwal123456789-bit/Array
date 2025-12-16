baf4a6ffd1df29123cba47584392b3603a77f0fe
Ujjwal Sharma <sharmaujjwal123456789@gmail.com>
2025-12-16 22:58:22 +0530

    some change in arr34

--- Arr34.java
+++ Arr34.java
@@ -1,25 +1,26 @@
-import java.util.*;
-public class Arr34 {
-    public static long nCr(int n, int r){
-        long res=1;
+//  finding the element in pascal triangle in nth row and nth column
+// import java.util.*;
+// public class Arr34 {
+//     public static long nCr(int n, int r){
+//         long res=1;
 
-        for(int i=0;i<r;i++){
-            res = res *  (n-i);
-            res = res / (i+1);
-        }
-        return res;
-    }
-    public static void main(String[] args){
-        Scanner sc =new Scanner(System.in);
+//         for(int i=0;i<r;i++){
+//             res = res *  (n-i);
+//             res = res / (i+1);
+//         }
+//         return res;
+//     }
+//     public static void main(String[] args){
+//         Scanner sc =new Scanner(System.in);
 
-        System.out.println("Enter the row no.");
-        int n= sc.nextInt();
+//         System.out.println("Enter the row no.");
+//         int n= sc.nextInt();
 
-        System.out.println("Enter the element in column");
-        int r=sc.nextInt();
+//         System.out.println("Enter the element in column");
+//         int r=sc.nextInt();
          
-        long result = nCr(n, r);
-        System.out.println("the result is "+ result);
+//         long result = nCr(n, r);
+//         System.out.println("the result is "+ result);
 
-    }
-}
+//     }
+// }
