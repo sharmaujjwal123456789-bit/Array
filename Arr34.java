f8cecd76d04a23c3fd76ae99d234892ce89d9b08
Ujjwal Sharma <sharmaujjwal123456789@gmail.com>
2025-12-16 22:55:30 +0530

    Arr34 nCr

---
+++ Arr34.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Arr34 {
+    public static long nCr(int n, int r){
+        long res=1;
+
+        for(int i=0;i<r;i++){
+            res = res *  (n-i);
+            res = res / (i+1);
+        }
+        return res;
+    }
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+
+        System.out.println("Enter the row no.");
+        int n= sc.nextInt();
+
+        System.out.println("Enter the element in column");
+        int r=sc.nextInt();
+         
+        long result = nCr(n, r);
+        System.out.println("the result is "+ result);
+
+    }
+}
