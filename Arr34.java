e8b6673a933f13024870c66d9a2424ec2741a2f6
Ujjwal Sharma <sharmaujjwal123456789@gmail.com>
2025-12-17 00:32:55 +0530

    Arr34

--- Arr34.java
+++ Arr34.java
@@ -4,10 +4,10 @@
 //     public static long nCr(int n, int r){
 //         long res=1;
 
-//         for(int i=0;i<r;i++){
+//         for(int i=1;i<r;i++){
 //             res = res *  (n-i);
-//             res = res / (i+1);
-//         }
+//             res = res / (i);
+//         }  
 //         return res;
 //     }
 //     public static void main(String[] args){
@@ -24,3 +24,58 @@
 
 //     }
 // }
+
+
+//  print the pascal triangle full row
+//  import java.util.*;
+//  public class Arr34{
+//     public static void row(int n){
+
+//         int ans=1;
+//         System.out.print(ans);
+//         for(int i=1 ;i< n;i++){
+//             ans= ans * (n-i);
+//             ans= ans / (i);
+//             System.out.print(" " + ans);
+//         }
+//     }
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the row to be printed");
+//         int n= sc.nextInt();
+//         row(n);
+//     }
+//  }
+
+
+// for printing the pascal full triangle 
+import java.util.*;
+public class Arr34{
+    public static ArrayList<Integer> generateRow(int row){
+
+        long ans=1;
+        ArrayList<Integer> ansRow =new ArrayList<>();
+        ansRow.add(1);
+
+        for(int col =1 ; col< row ; col++){
+            ans = ans * (row - col);
+            ans = ans / (col);
+            ansRow.add((int)ans);
+        }
+        return ansRow;
+    }
+    public static void main(String[] args){
+
+   Scanner sc= new Scanner(System.in);
+   ArrayList<List<Integer>> ans = new ArrayList<>();
+
+   System.out.println("Enter the no. of rows");
+   int n= sc.nextInt();
+
+   for(int i=1;i<=n;i++){
+    ans.add(generateRow(i));
+   }
+
+   System.out.print(ans);
+ }     
+}
