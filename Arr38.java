e8a408017c1a9558729432ac4a6792b73b5b7e3f
Ujjwal Sharma <sharmaujjwal123456789@gmail.com>
2026-01-24 00:37:08 +0530

    arr38

--- Arr38.java
+++ Arr38.java
@@ -1,3 +1,33 @@
-public class Arr38 {
+import java.util.*;
+public  class Arr38 {
+    public static int subArray(int []nums,int n,int target){
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            int xor=0;
+            for(int j=i;j<n;j++){
+                xor=xor^nums[j];
+
+                if(xor == target){
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
     
+  public static void main(String []args){
+     Scanner sc=new Scanner(System.in);
+     System.out.println("Enter the sum of array  to be founded");
+ 
+     int target =sc.nextInt();
+
+    int [] nums= {4,2,2,6,4};
+    int n =nums.length;
+
+    int result = subArray(nums,n,target);
+
+   System.out.println(result);
+
+  }
 }
+ 
